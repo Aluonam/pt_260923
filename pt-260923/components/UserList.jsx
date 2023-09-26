7ea7bf64a094c55d64ef1977f55320a25e437633
Aluon <paulacanoroman@gmail.com>
2023-09-26 12:55:18 +0200

    feat: consumo API

--- pt-260923/components/UserList.jsx
+++ pt-260923/components/UserList.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 const UserList = () => {
 
+    const [usersData, setUsersData] = useState([])
+
+    useEffect(() => {
+      apiCall()
+    }, [])
+
+    const apiCall = async () =>{
+        try{
+            const call = await fetch(`https://randomuser.me/api/?results=50`);
+            const data = await call.json();
+            setUsersData(data.results)
+
+        }catch(error){"error detected", error}
+    }
     
   return (
     <div>UserList</div>
